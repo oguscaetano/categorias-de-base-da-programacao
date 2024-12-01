c8fdcdb5db9e3e4c66078a3a0b4ad5009ce25460
Gustavo Caetano <gustavo.caetano13@gmail.com>
2024-12-01 17:20:46 -0300

    setup live 14 - eventos

---
+++ 10-metodos-de-array/data.js
@@ -0,0 +1,175 @@
+const data = {
+  "characters": [
+    {
+      "id": 1,
+      "name": "SON GOKU",
+      "gender": "M",
+      "speciesId": 1,
+      "powers": ["Dragon Flash Fist", "Kamehameha", "Rapid Kick Rush"],
+      "image": "https://dragonball-api.com/characters/goku_normal.webp"
+    },
+    {
+      "id": 2,
+      "name": "VEGETA",
+      "gender": "M",
+      "speciesId": 1,
+      "powers": ["Crusher Knee Kick", "Ki Blast Rush", "Super Dash Kick"],
+      "image": "https://dragonball-api.com/characters/vegeta_normal.webp"
+    },
+    {
+      "id": 3,
+      "name": "MAJIN BOO",
+      "gender": "M",
+      "speciesId": 2,
+      "powers": ["Cartwheel", "Mount Drive", "Potbelly Attack"],
+      "image": "https://dragonball-api.com/characters/BuuGordo_Universo7.webp"
+    },
+    {
+      "id": 4,
+      "name": "FREEZA",
+      "gender": "M",
+      "speciesId": 3,
+      "powers": ["Death Xucer", "Earth Breaker", "Next Time"],
+      "image": "https://dragonball-api.com/characters/Freezer.webp"
+    },
+    {
+      "id": 5,
+      "name": "CELL",
+      "gender": "M",
+      "speciesId": 4,
+      "powers": ["Hell Strike", "Perfect Attack", "Telekinesis Crush"],
+      "image": "https://dragonball-api.com/characters/celula.webp"
+    },
+    {
+      "id": 6,
+      "name": "ANDROIDE 18",
+      "gender": "F",
+      "speciesId": 5,
+      "powers": ["Back Grab", "Barrier", "Destructo-Disc"],
+      "image": "https://dragonball-api.com/characters/Androide_18_Artwork.webp"
+    },
+    {
+      "id": 7,
+      "name": "ANDROIDE 16",
+      "gender": "M",
+      "speciesId": 5,
+      "powers": ["Dynamite Driver", "Flying Powerbomb", "Flying Slide Powerbomb"],
+      "image": "https://dragonball-api.com/characters/Androide_16.webp"
+    },
+    {
+      "id": 8,
+      "name": "TRUNKS",
+      "gender": "M",
+      "speciesId": 1,
+      "powers": ["Change the Future", "Cyclone Buster", "Masenko"],
+      "image": "https://dragonball-api.com/characters/Trunks_Buu_Artwork.webp"
+    },
+    {
+      "id": 9,
+      "name": "GOHAN",
+      "gender": "M",
+      "speciesId": 1,
+      "powers": ["Dragon Flight", "Aerial Kick", "Kamehameha"],
+      "image": "https://dragonball-api.com/characters/gohan.webp"
+    },
+    {
+      "id": 10,
+      "name": "SENHOR POPO",
+      "gender": "M",
+      "speciesId": 6,
+      "powers": ["Unknown"],
+      "image": "https://glben.dokkaninfo.com/assets/global/en/character/thumb/card_1003440_thumb.png"
+    },
+    {
+      "id": 11,
+      "name": "PICCOLO",
+      "gender": "M",
+      "speciesId": 7,
+      "powers": ["Demon Elbow", "Demon Slicer", "Homing Energy Blast"],
+      "image": "https://dragonball-api.com/characters/picolo_normal.webp"
+    },
+    {
+      "id": 12,
+      "name": "KAMI-SAMA",
+      "gender": "M",
+      "speciesId": 7,
+      "powers": ["Unknown"],
+      "image": "https://imagensemoldes.com.br/wp-content/uploads/2020/06/Kami-Sama-Goku-Transparente-PNG.png"
+    },
+    {
+      "id": 13,
+      "name": "KURIRIN",
+      "gender": "M",
+      "speciesId": 8,
+      "powers": ["Afterimage Strike", "Kamehameha", "Solar Flare"],
+      "image": "https://dragonball-api.com/characters/Krilin_Universo7.webp"
+    },
+    {
+      "id": 14,
+      "name": "BULMA",
+      "gender": "F",
+      "speciesId": 8,
+      "powers": [],
+      "image": "https://dragonball-api.com/characters/bulma.webp"
+    },
+    {
+      "id": 15,
+      "name": "MESTRE KAME",
+      "gender": "M",
+      "speciesId": 8,
+      "powers": ["Kamehameha"],
+      "image": "https://dragonball-api.com/characters/roshi.webp"
+    },
+    {
+      "id": 16,
+      "name": "VIDEL",
+      "gender": "F",
+      "speciesId": 8,
+      "powers": [],
+      "image": "https://i.pinimg.com/originals/34/e9/14/34e91403dc8976df0224e022abd2d700.png"
+    },
+    {
+      "id": 17,
+      "name": "YAMCHA",
+      "gender": "M",
+      "speciesId": 8,
+      "powers": ["Wolf Fang Fist", "Gale Claws", "Kamehameha"],
+      "image": "https://dragonball-api.com/characters/Final_Yamcha.webp"
+    }
+  ],
+
+  "species": [
+    {
+      "id": 1,
+      "name": "Saiyans"
+    },
+    {
+      "id": 2,
+      "name": "Majin"
+    },
+    {
+      "id": 3,
+      "name": "Ciborgue"
+    },
+    {
+      "id": 4,
+      "name": "Bio-Androide"
+    },
+    {
+      "id": 5,
+      "name": "Androide"
+    },
+    {
+      "id": 6,
+      "name": "Desconhecido"
+    },
+    {
+      "id": 7,
+      "name": "Namekuseijin"
+    },
+    {
+      "id": 8,
+      "name": "Terráqueo"
+    }
+  ]
+}
